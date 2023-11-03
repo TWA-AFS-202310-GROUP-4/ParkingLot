a11eff962533f0711c98e0808690d6d0fc06005c
Xinwen Xiang <XXiang2@slb.com>
2023-11-03 16:16:45 +0800

    feat: implement  get a car when give a ticket from parking, AC1 done

---
+++ ParkingLot/Parking/Parking.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingLot.Parking
+{
+    public class Parking
+    {
+        public string FectchCar(string ticket)
+        {
+            return "car";
+        }
+
+        public string Park(string car)
+        {
+            return "ticket";
+        }
+    }
+}
---
+++ ParkingLotTest/ParkingLotTest.cs
@@ -0,0 +1,28 @@
+﻿using ParkingLot.Parking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ParkingLotTest
+{
+    public class ParkingLotTest
+    {
+        [Fact]
+        public void Should_fetch_a_car_when_fetchCar_with_ticket()
+        {
+            //given
+            Parking park = new Parking();
+            string ticket = park.Park("car");
+
+            //when
+            string car = park.FectchCar(ticket);
+
+            //then
+            Assert.Equal("car", car);
+        }
+    }
+}
