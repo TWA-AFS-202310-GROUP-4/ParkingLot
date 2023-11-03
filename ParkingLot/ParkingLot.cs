4e1fd21146fe061fe9d3bef296c41514e9df88d1
Diana Jing <DJing@slb.com>
2023-11-03 16:22:18 +0800

    feat: complete the function that a customer can fetch his parked car with test case

---
+++ ParkingLot/ParkingLot.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parking
+{
+    public class ParkingLot
+    {
+        private string carPlate;
+        private string ticket;
+
+        public string Fetch(string ticket)
+        {
+            return carPlate;
+        }
+
+        public string Park(string car)
+        {
+            this.carPlate = car;
+            return ticket;
+        }
+    }
+}
---
+++ ParkingLotTest/ParkingLotTest.cs
@@ -0,0 +1,19 @@
+﻿using Parking;
+using Xunit;
+
+namespace ParkingLotTest
+{
+    public class ParkingLotTest
+    {
+        [Fact]
+        public void Should_get_same_car_when_fetch_car_by_ticket()
+        {
+            string car = "car1";
+            var parkingLot = new Parking.ParkingLot();
+            string ticket = parkingLot.Park(car);
+            string result = parkingLot.Fetch(ticket);
+
+            Assert.Equal("car1", result);
+        }
+    }
+}
